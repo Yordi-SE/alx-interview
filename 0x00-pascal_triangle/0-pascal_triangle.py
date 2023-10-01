72dc0013760fde1e40483327ac462209de215a72
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-10-01 16:21:00 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""pascal triangle function"""
+
+
+def pascal_triangle(n):
+    main_list = [[1], [1, 1]]
+    sub_list = []
+    for j in range(n - 2):
+        for i in range(len(main_list[-1]) - 1):
+            sub_list.append(main_list[-1][i] + main_list[-1][i + 1])
+        sub_list.append(1)
+        sub_list.insert(0, 1)
+        main_list.append(sub_list)
+        sub_list = []
+    return main_list
